edbfa14fef2bd4064cf4363f0a37dd00bb391be2
Wonder <106922826+Marc-Aradillas@users.noreply.github.com>
2022-06-30 15:55:55 -0500

    Update Monkey.java

--- Monkey.java
+++ Monkey.java
@@ -1,44 +1,28 @@
-//SNHU IT 145
-//5-4 Milestone: Menu Loop and Monkey Class
-//Monkey.Java inherits from ResuceAnimal
-//Student: Marc A. Aradillas
-//2022
+/*	SNHU IT145
+ *	7-1 Project Two Submission: Driver and Monkey Class
+ *	Student: Marc A. Aradillas
+ *	2022
+ */
 
 public class Monkey extends RescueAnimal {
 	
-	//instance variables
-	private String tailLength;
-	private String height;
-	private String bodyLength;
+	//Class Variables
+	
+	private double tailLength;
+	private double height;
+	private double bodyLength;
 	private String species;
-	private String breed;
-	//TODO: declare variables for height, bodyLength, species, and breed
 	
-	//constructors
 	
-	// (default empty argument constructor not required for  5-4 milestone)
-	public Monkey() {
-		tailLength = "";
-		//TODO: initialize height, bodyLength, species, and breed
-	}
+	// Constructor
+	// made changes to variable types to reflect measurement with decimals
 	
-	//to score exemplary on this criterion
-	//you must also include a more detailed constructor that takes
-	//values for all Monkey's attributes and sets them.
-	//Refer to the constructor in Dog Class for an example.
-	
-	//Accomplished: include parameters-arguments for height, bodyLength, species, and breed
-	public Monkey(String name, String tailLength, String height, String bodyLength,
-			String species, String breed, String gender, String age, String weight,
+	public Monkey(String name, double tailLength, double height, double bodyLength,
+			String species, String gender, String age, String weight,
 			String acquisitionDate, String acquisitionCountry, String trainingStatus,
 			boolean reserved, String inServiceCountry) {
+		
 		setName(name);
-		//Accomplished: create and call setTailLength() method
-		setTailLength(tailLength);
-		setHeight(height);
-		setBodyLength(bodyLength);
-		setSpecies(species);
-		setBreed(breed);
 		setGender(gender);
 		setAge(age);
 		setWeight(weight);
@@ -47,30 +31,34 @@ public class Monkey extends RescueAnimal {
 		setTrainingStatus(trainingStatus);
 		setReserved(reserved);
 		setInServiceCountry(inServiceCountry);
-		//Accomplished: create and call setters for height, bodyLength, species, and breed
+		
+		setTailLength(tailLength);
+		setHeight(height);
+		setBodyLength(bodyLength);
+		setSpecies(species);
 	}
 
-	public String getTailLength() {
+	public double getTailLength() {
 		return tailLength;
 	}
 
-	public void setTailLength(String tailLength) {
+	public void setTailLength(double tailLength) {
 		this.tailLength = tailLength;
 	}
 
-	public String getHeight() {
+	public double getHeight() {
 		return height;
 	}
 
-	public void setHeight(String height) {
+	public void setHeight(double height) {
 		this.height = height;
 	}
 
-	public String getBodyLength() {
+	public double getBodyLength() {
 		return bodyLength;
 	}
 
-	public void setBodyLength(String bodyLength) {
+	public void setBodyLength(double bodyLength) {
 		this.bodyLength = bodyLength;
 	}
 
@@ -81,23 +69,6 @@ public class Monkey extends RescueAnimal {
 	public void setSpecies(String species) {
 		this.species = species;
 	}
-
-	public String getBreed() {
-		return breed;
-	}
-
-	public void setBreed(String breed) {
-		this.breed = breed;
-	}
-
-	
-
-	
-	//Accessors (getter)Methods
-	//TODO: create getters for all Monkey's attributes
-	
-	//Mutator (setter) Methods
-	//TODO: create setters foir all Monkey's attributes
 	
 	
 }
